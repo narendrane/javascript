953c7a31897ceb80a50f30981c5fe55fb70e24c5
Narendran Ethiraj <narendrn@gmail.com>
2024-01-09 16:12:07 +1100

    Update functionCurrying.js

--- functionCurrying.js
+++ functionCurrying.js
@@ -16,3 +16,15 @@ function curry(fn) {
 }
 const curriedSum = curry(sum);
 console.log(curriedSum(2)(3)(4));
+console.log("********** Function Currying - Logging **********");
+function log(date, importance, message) {
+  return `[${date.getHours()}:${date.getMinutes()}], [${importance}], ${message}`;
+}
+log(new Date(), "INFO", "This is a message");
+
+const curriedLog = curry(log);
+console.log(curriedLog(new Date())("ERROR")("Firstname cannot be null"));
+
+const logNow = curriedLog(new Date());
+console.log(logNow("ERROR")("Lastname cannot be null"));
+console.log(logNow("INFO")("User logged out!"));
