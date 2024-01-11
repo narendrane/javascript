d51d8266886673ea5c1e8599c7eb9bdf67fffcf4
Narendran Ethiraj <narendrn@gmail.com>
2024-01-11 18:40:35 +1100

    Update generators.js

--- generators.js
+++ generators.js
@@ -1,3 +1,28 @@
-/**
+/** 
 Generators - are special class of function that simplify the task of writing Iterators
+A Generator function can "pause" the execution
+'yield' is used to pause the execution and yields a value
+Each Generator object may only be iterated once
+- return sets the done flag to true after which the generator cannot generate anymore values.
 */
+function* generators(){
+  yield  'Hello'
+  yield  'World'
+}
+const generator = generators();
+for(const word of generator){
+  console.log(word)
+}
+console.log([...generator]);
+
+
+// Generator with return
+function* generators1(){
+  yield  'Hello'
+  return 'World'
+}
+const generator1 = generators1();
+for(const word of generator1){
+  console.log(word)
+}
+console.log(generator1.next());
