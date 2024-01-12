3fb8bb5a78513cc2bb7d5441c74fe4336267c6ea
Narendran Ethiraj <narendrn@gmail.com>
2024-01-12 18:48:40 +1100

    Update event-looping.js

--- event-looping.js
+++ event-looping.js
@@ -2,12 +2,12 @@
 Event Loop
 
 The JS Environment consists of
-1. Memory Heap
-2. Call Stack
-3. Web API's
-4. Event Loop
-5. Callback queue / Task queue
-6. Microtask Queue - 
+1. Memory Heap - Whenever a variable or function is declared memory is allocated in the heap.
+2. Call Stack - Whenever a function is called, the function is pushed into the call stack. when returns it is popped out from the stack.
+3. Web APIs - Browser functions like setTimeout, setInterval, XMLHttpRequest, etc.
+4. Event Loop - Checks if the call stack is empty and if the call stack is empty, checks if there are any pending tasks in the event loop. If there are any pending tasks, it pushes the task into the call stack.
+5. Callback queue / Task queue - when the Web APIs are done, they are pushed into the callback queue.
+6. Microtask Queue - when the callback queue is done, it pushes the microtasks into the microtask queue. such as fulfilling promise values.
 */
 function eventLoop() {
   setTimeout(() => {
@@ -24,3 +24,4 @@ function eventLoop() {
   console.log("Final Console");
 }
 eventLoop();
+
