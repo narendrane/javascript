006d12d5117181016be305f49cbedd5d5134745c
Narendran Ethiraj <narendrn@gmail.com>
2024-01-12 15:07:31 +1100

    Update promise.js

--- promise.js
+++ promise.js
@@ -7,7 +7,7 @@ Higher order function (HOC) - A function that takes another function as an argum
 Synchronous callback - A callback function that is executed Immediately.
 example: sort, filter, map, forEach, reduce, find, every, some, findIndex, findLastIndex
 
-Asynchrous callback - A Asynchrous callback is a callback that is often used to continue or resume code execution after an synchronous operation has completed.
+Asynchronous callback - An Asynchronous callback is a callback that is often used to continue or resume code execution after a synchronous operation has been completed.
 Example: setTimeout, setInterval
 
 callback hell - A callback hell is a nested callback function that is difficult to read and maintain.
@@ -26,7 +26,7 @@ function higerOrderFunction(callback) {
 
 higerOrderFunction(greet);
 
-// here the callback is attached only on click. (Asynchrous))
+//Here the callback is attached only on click. (Asynchrous))
 function callBackFunction() {
   console.log(i);
   console.log("You clicked on button # " + i);
@@ -54,14 +54,14 @@ const onRejection = (result) => {
 const promise = new Promise((resolve, reject) => {
   let result = false;
   setTimeout(() => {
-    // change the status to fulfilled
+    //Change the status to fulfilled
     result = true;
     resolve(result);
   }, 2000);
 
   //  to reject
   setTimeout(() => {
-    // change the status to rejected
+    //Change the status to rejected
     reject(result);
   }, 2000);
 });
@@ -74,3 +74,38 @@ promise.then(onFulfillment);
 //when rejected catch() will be executed
 // catch will also catch the errors from fulfillment
 promise.catch(onRejection);
+
+//  Chaining Promises
+// can be written as promise.then(onFulfillment).catch(onRejection);
+//  Promise.then()
+//.then()
+//.then()
+
+/** 
+Promise Static methods
+1. Promise.all() - waits for all promises to resolve and returns the array of their results. If any one fails, it becomes the error
+2. Promise.allSettled() - waits for all the promises to settle regardless of whether or not one of them is rejected.
+3. Promise.race() - waits for the first promise to settle and its result/error becomes the outcome.
+*/
+const promise1 = Promise.resolve(3);
+const promise2 = 43;
+const promise3 = new Promise((resolve, reject) => {
+  setTimeout(resolve, 100, "foo");
+});
+Promise.all([promise1, promise2, promise3])
+  .then((values) => {
+    console.log(values);
+  })
+  .catch((error) => {
+    console.log("Error " + error);
+  });
+// outputs [3, 43, "foo"]
+
+Promise.allSettled([promise1, promise2, promise3]).then((values) => {
+  console.log(values);
+});
+
+Promise.race([promise1, promise2, promise3]).then((values) => {
+  console.log("Race: ", values);
+});
+
